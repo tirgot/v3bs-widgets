501953c69d2ad3402d2dce5cd7fa87d80b85eb08
tirgot <miles@tirgot.com>
2021-03-03 17:09:29 -0800

    cmt-20210303-001
    Add MultiSelector widget

---
+++ src/widgets/form/MultiSelector.vue
@@ -0,0 +1,30 @@
+<template>
+  <Multiselect v-model="val_arr" :options="options" mode="tags"></Multiselect>
+</template>
+
+<script>
+import Multiselect from "@vueform/multiselect"
+
+export default {
+  name: "MultiSelector",
+  components: { Multiselect },
+  props: ['modelValue', 'options'],
+  data(){
+    return {
+      val_arr: [0]
+    }
+  },
+  emits: ['update:modelValue'],
+  watch: {
+    val_arr(newVal, oldVal){
+      this.$emit('update:modelValue', newVal)
+    }
+  },
+  created(){
+    this.val_arr = this.modelValue
+  }
+}
+
+</script>
+
+<style src="@vueform/multiselect/themes/default.css"></style>
